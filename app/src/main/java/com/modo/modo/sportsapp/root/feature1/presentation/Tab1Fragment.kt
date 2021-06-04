5cc12b25900eefaa4749221bb71822c9efd2642c
leonid.belyakov <leonid.belyakov@e-legion.com>
2021-06-05 01:49:20 +0400

    чтение qr

--- app/src/main/java/com/modo/modo/sportsapp/root/feature1/presentation/Tab1Fragment.kt
+++ app/src/main/java/com/modo/modo/sportsapp/root/feature1/presentation/Tab1Fragment.kt
@@ -1,7 +1,21 @@
 package com.modo.modo.sportsapp.root.feature1.presentation
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.modo.modo.sportsapp.R
+import com.modo.modo.sportsapp.databinding.FragmentTab1Binding
 
 class Tab1Fragment : Fragment(R.layout.fragment_tab1) {
+
+    private val binding by viewBinding(FragmentTab1Binding::bind)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.root.setOnClickListener {
+            requireActivity().findNavController(R.id.activityContent).navigate(R.id.cameraFragment)
+        }
+    }
 }
