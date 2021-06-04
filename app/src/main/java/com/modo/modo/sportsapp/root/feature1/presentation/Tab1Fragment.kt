1d6ca1658b05b22ea978ff7678565c7f3f82900d
leonid.belyakov <leonid.belyakov@e-legion.com>
2021-06-04 23:49:09 +0400

    навигация по табам

---
+++ app/src/main/java/com/modo/modo/sportsapp/root/feature1/presentation/Tab1Fragment.kt
@@ -0,0 +1,7 @@
+package com.modo.modo.sportsapp.root.feature1.presentation
+
+import androidx.fragment.app.Fragment
+import com.modo.modo.sportsapp.R
+
+class Tab1Fragment : Fragment(R.layout.fragment_tab1) {
+}
